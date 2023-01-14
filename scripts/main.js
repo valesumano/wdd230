9a6aa81f2457c4c2905663d052ce2c6462921a82
valesumano <valesumano@hotmail.com>
2023-01-14 12:53:54 -0700

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 let date = new Date();
 let year = date.getFullYear();
 
-document.querySelector('h1').innerHTML = '&copy;' + year + '<h4>test</h4>';
+document.querySelector('.year').innerHTML = '&copy;' + year;
 
 let currentdate = document.lastModified;
 console.log(currentdate);
 
-document.querySelector('div').textContent = `Last Updated: ${currentdate}`;
+document.querySelector('.updated').textContent = currentdate;
