f8cf3979307552eb0982e28f1529b3e3dbf92984
valesumano <valesumano@hotmail.com>
2023-01-30 15:24:45 -0700

    update chamber

--- chamber/styles/chamber.js
+++ chamber/styles/chamber.js
@@ -17,3 +17,13 @@ hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('responsive');
 })
 
+
+let date = new Date();
+let year = date.getFullYear();
+
+document.querySelector('.year').innerHTML = '&copy;' + year;
+
+let currentdate = document.lastModified;
+console.log(currentdate);
+
+document.querySelector('.updated').textContent = currentdate;
