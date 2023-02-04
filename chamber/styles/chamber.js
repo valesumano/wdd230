3d1465e6b5efc88ac69a3d151a74b74115a6c2e1
valesumano <valesumano@hotmail.com>
2023-02-04 13:13:31 -0700

    img

--- chamber/styles/chamber.js
+++ chamber/styles/chamber.js
@@ -10,12 +10,13 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 datefield.innerHTML = `<em>${fulldate}</em>`;
 
 //Hamburger Menu
-const mainnav = document.querySelector('.navigation');
+const mainnav = document.querySelector('#nav');
 const hambutton = document.querySelector('.ham');
 
 hambutton.addEventListener('click', () => {
-    mainnav.classList.toggle('responsive');
+    mainnav.classList.toggle('active');
 })
+
 
 
 let date = new Date();
@@ -31,11 +32,31 @@ document.querySelector('.updated').textContent = currentdate;
 
 //Banner
 
-let fulldate = new Date();
-let weekday =date.getDay();
-if (weekday == 1|| weekday == 2){
-    show
-}
+// let fulldate = new Date();
 
 
+// let weekday =date.getDay();
+// if (weekday == 1|| weekday == 7){
+//     show.innerHTML= `<p>🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.</p>`;
+// }
+
+
+
+
+// var weekday = new Array(7)
+
+// weekday[0] = "spectacular Sunday"
+// weekday[1] = "marvelous Monday"
+// weekday[2] = "terrific Tuesday"
+// weekday[3] = "wonderful Wednesday"
+// weekday[4] = "totally cool Thursday"
+// weekday[5] = "fantastic Friday"
+// weekday[6] = "sweet Saturday"
+
+// var currentDate = new Date()
+// weekdayValue = currentDate.getDay()
+
+// document.write(
+//     '<p>Have a ' + weekday[weekdayValue] + '!</p>'
+//   )
 
