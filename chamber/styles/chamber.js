0fdeeddc1f29eb65ccc827cf86df8448ff4bebe4
valesumano <valesumano@hotmail.com>
2023-02-10 19:23:21 -0700

    update

--- chamber/styles/chamber.js
+++ chamber/styles/chamber.js
@@ -34,3 +34,17 @@ else {
 
 
 
+//Weather calculator
+const temp = document.querySelector('.t').textContent;
+const ws = document.querySelector('.ws').textContent;
+
+let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(ws,0.16)) + (0.4275*temp*Math.pow(ws,0.16)));
+
+let windchill = document.querySelector('.wc');
+if (temp <= 50 && ws >= 3) {
+    windchill.textContent = chill;
+}
+
+
+
+
