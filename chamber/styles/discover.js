26865906a5bb0e88a268129bdd14173906672456
valesumano <valesumano@hotmail.com>
2023-02-16 15:15:32 -0700

    discover

--- chamber/styles/discover.js
+++ chamber/styles/discover.js
@@ -1,12 +1,58 @@
+const images = document.querySelectorAll('[data-src]');
+
+function preloadImage(img){
+    const source = img.getAttribute('data-src');
+    if(!source) {
+        return;
+    }
+
+    img.src = source;
+}
+
+const options = {
+    threshold: .5,
+    rootMargin: "-50px"
+};
+
+const io = new IntersectionObserver (
+    (entries, io) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                console.log(entry.target);
+                preloadImage(entry.target);
+                io.unobserve(entry.target); 
+            }
+        });     
+    }, options
+);
+
+
+images.forEach(image => {
+    io.observe(image);
+});
+
+
 
 //if there is nothing in local storage, go this
 if(!localStorage.getItem('lastVisit')) {
     localStorage.setItem('lastVisit', Date.now())
-    
+    document.getElementById('diff').textContent = 'This is your 1st visit';
+} else {
+    setStyles();
+}
 
-} else{
-    let currentdate = Date.now(),
-    let lastdate = localStorage.getItem('lastVisit')
-    let difference = (currentdate - lastdate) /1000/60/60/24
-    console.log(difference);
+function setStyles() {
+    let prevDate = localStorage.getItem('lastvisit');
+    let currDate = Date.now();
+    
+    let difference = currDate - prevDate;
+        console.log(difference);
+        let daysDifference = Math.floor(difference/1000/60/60/24);
+
+    document.getElementById('diff').textContent = daysDifference;
+
+    localStorage.setItem('lastvisit', Date.now());
+
 }
