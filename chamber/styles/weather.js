326582a9bc269af9908c9445b9787496b998eabb
valesumano <valesumano@hotmail.com>
2023-03-13 13:15:05 -0600

    chill

--- chamber/styles/weather.js
+++ chamber/styles/weather.js
@@ -14,7 +14,15 @@ const getWeather = async () => {
     document.querySelector('#weather_icon').alt= data.currentConditions.conditions + ' icon';
 
 
-    //windchill information here
+    const temp = document.querySelector('#t').textContent;
+    const ws = document.querySelector('#ws').textContent;
+
+    let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(ws,0.16)) + (0.4275*temp*Math.pow(ws,0.16)));
+
+    let windchill = document.querySelector('#wc');
+    if (temp <= 50 && ws >= 3) {
+    windchill.textContent = chill;
+}
   };
 getWeather();
 
