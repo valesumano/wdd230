1780811ca87e26e0f661fbd246f9846ff144d314
valesumano <valesumano@hotmail.com>
2023-03-31 16:38:01 -0600

    push

--- foods/styles/fresh2.js
+++ foods/styles/fresh2.js
@@ -54,92 +54,72 @@ const date = dayOfWeek + ", " + day + " " + month + " " + year;
 //Submition
 
 
+// Get the form element
+const form = document.querySelector('form');
+
+// Add an event listener for form submission
+form.addEventListener('submitForm', (event) => {
+  event.preventDefault(); // Prevent the default form submission behavior
+  
+  // Get the input values
+  const firstName = document.querySelector('#fname').value;
+  const email = document.querySelector('#email').value;
+  const phone = document.querySelector('#phone').value;
+  const fruits = Array.from(document.querySelectorAll('input[name="fruit"]:checked')).map(fruit => fruit.value);
+  const instructions = document.querySelector('#instructions').value;
+  
+  // Get the order date
+  const date = new Date();
+  const orderDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  
+  // Calculate the total nutrition information
+  let totalCarbs = 0;
+  let totalProtein = 0;
+  let totalFat = 0;
+  let totalSugar = 0;
+  let totalCalories = 0;
+  
+  // Loop through the selected fruits and fetch their nutrition data
+  fruits.forEach((fruit) => {
+    fetch(`https://andejuli.github.io/wdd230/fruit.json`)
+      .then(response => response.json())
+      .then(data => {
+        const selectedFruit = data.find(f => f.name === fruit);
+        if (selectedFruit) {
+          totalCarbs += selectedFruit.nutritions.carbohydrates;
+          totalProtein += selectedFruit.nutritions.protein;
+          totalFat += selectedFruit.nutritions.fat;
+          totalSugar += selectedFruit.nutritions.sugar;
+          totalCalories += selectedFruit.nutritions.calories;
+        }
+      })
+      .then(() => {
+        // Update the output area
+        const outputArea = document.querySelector('.output-area');
+        outputArea.innerHTML = `
+          <h2>Form Values:</h2>
+          <hr>
+          <p>First Name: ${firstName}</p>
+          <p>Email: ${email}</p>
+          <p>Phone Number: ${phone}</p>
+          <p>Fruits: ${fruits.join(', ')}</p>
+          <p>Special Instructions: ${instructions}</p>
+          <br>
+          <h2>Nutrition Information</h2>
+          <hr>
+          <p>Carbohydrates: ${totalCarbs.toFixed(2)}</p>
+          <p>Protein: ${totalProtein.toFixed(2)}</p>
+          <p>Fat: ${totalFat.toFixed(2)}</p>
+          <p>Sugar: ${totalSugar.toFixed(2)}</p>
+          <p>Calories: ${totalCalories.toFixed(2)}</p>
+          <br>
+          <i>
+              <p>Order Date: ${orderDate}</p>
+          </i>
+        `;
+      })
+
+  });
+});
 
 
-// const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-// const fruitUrl = 'https://andejuli.github.io/wdd230/fruit.json';
-// const fetchPromise = fetch(proxyUrl + fruitUrl)
-//   .then(response => response.json())
-//   .then(data => {
-//     const fruit = data.find(f => f.name === checkbox.value);
-//     if (fruit) {
-//       // update total nutrition information
-//     }
-//   })
-//   .catch(error => console.log(error));
-
-
-// function submitForm(event) {
-//   event.preventDefault();
-//   const date = new Date();
-//   const orderDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-//   let numDrinks = localStorage.getItem('numDrinks') || 0;
-
-//   const eachCheckbox = document.getElementsByName('checkbox');
-//   const allMarkedBoxes = [];
-//   let totalCarbs = 0;
-//   let totalProtein = 0;
-//   let totalFat = 0;
-//   let totalSugar = 0;
-//   let totalCalories = 0;
-
-//   const fetchPromises = [];
-
-//   for (const checkbox of eachCheckbox) {
-//     if (checkbox.checked) {
-//       allMarkedBoxes.push(checkbox.value);
-//       const fetchPromise = fetch('https://andejuli.github.io/wdd230/fruit.json')
-//         .then((response) => {
-//           if (!response.ok) {
-//             throw new Error('Network response was not ok');
-//           }
-//           return response.json();
-//         })
-//         .then((data) => {
-//           const fruit = data.find((f) => f.name === checkbox.value);
-//           if (fruit) {
-//             totalCarbs += fruit.nutritions.carbohydrates;
-//             totalProtein += fruit.nutritions.protein;
-//             totalFat += fruit.nutritions.fat;
-//             totalSugar += fruit.nutritions.sugar;
-//             totalCalories += fruit.nutritions.calories;
-//           } else {
-//             throw new Error(`Could not find nutrition information for ${checkbox.value}`);
-//           }
-//         })
-//         .catch((error) => {
-//           console.error(error);
-//           alert(`An error occurred while fetching nutrition information for ${checkbox.value}: ${error.message}`);
-//         });
-//       fetchPromises.push(fetchPromise);
-//     }
-//   }
-
-//   Promise.allSettled(fetchPromises).then(() => {
-//     document.querySelector('.summary').innerHTML = `
-//       <br>
-//       <h2>Form Values:</h2>
-//       <hr>
-//       <p>First Name: ${event.target.elements.fname.value}</p>
-//       <p>Email: ${event.target.elements.email.value}</p>
-//       <p>Phone Number: ${event.target.elements.phone.value}</p>
-//       <p>Fruits: ${allMarkedBoxes.join(', ')}</p>
-//       <p>Special Instructions: ${event.target.elements.instructions.value}</p>
-//       <br>
-//       <h2>Nutrition Information</h2>
-//       <hr>
-//       <p>Carbohydrates: ${totalCarbs.toFixed(2)}</p>
-//       <p>Protein: ${totalProtein.toFixed(2)}</p>
-//       <p>Fat: ${totalFat.toFixed(2)}</p>
-//       <p>Sugar: ${totalSugar.toFixed(2)}</p>
-//       <p>Calories: ${totalCalories.toFixed(2)}</p>
-//       <br>
-//       <i>
-//           <p>Order Date: ${orderDate}</p
-//           <p>Drink Mixes Ordered: ${numDrinks}</p>
-//           </i>
-//         `
-//   })
-    
-//       localStorage.setItem(0, numDrinks);
-//     }
