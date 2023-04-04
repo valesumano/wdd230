96b96a3882a7e6450221bdf15e1440a77d2f286d
valesumano <valesumano@hotmail.com>
2023-04-04 12:22:05 -0600

    push

--- foods/styles/fresh2.js
+++ foods/styles/fresh2.js
@@ -53,73 +53,78 @@ const date = dayOfWeek + ", " + day + " " + month + " " + year;
 
 //Submition
 
+let totalCarbs = 0;
+let totalProtein = 0;
+let totalFat = 0;
+let totalSugar = 0;
+let totalCalories = 0;
+let numDrinks = localStorage.getItem("numDrinks") || 0;
 
-// Get the form element
-const form = document.querySelector('form');
+fetch('https://andejuli.github.io/wdd230/fruit.json')
+  .then(response => response.json())
 
-// Add an event listener for form submission
-form.addEventListener('submitForm', (event) => {
-  event.preventDefault(); // Prevent the default form submission behavior
-  
-  // Get the input values
-  const firstName = document.querySelector('#fname').value;
-  const email = document.querySelector('#email').value;
-  const phone = document.querySelector('#phone').value;
-  const fruits = Array.from(document.querySelectorAll('input[name="fruit"]:checked')).map(fruit => fruit.value);
-  const instructions = document.querySelector('#instructions').value;
-  
-  // Get the order date
-  const date = new Date();
-  const orderDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-  
-  // Calculate the total nutrition information
-  let totalCarbs = 0;
-  let totalProtein = 0;
-  let totalFat = 0;
-  let totalSugar = 0;
-  let totalCalories = 0;
-  
-  // Loop through the selected fruits and fetch their nutrition data
-  fruits.forEach((fruit) => {
-    fetch(`https://andejuli.github.io/wdd230/fruit.json`)
-      .then(response => response.json())
-      .then(data => {
-        const selectedFruit = data.find(f => f.name === fruit);
-        if (selectedFruit) {
-          totalCarbs += selectedFruit.nutritions.carbohydrates;
-          totalProtein += selectedFruit.nutritions.protein;
-          totalFat += selectedFruit.nutritions.fat;
-          totalSugar += selectedFruit.nutritions.sugar;
-          totalCalories += selectedFruit.nutritions.calories;
-        }
-      })
-      .then(() => {
-        // Update the output area
-        const outputArea = document.querySelector('.output-area');
-        outputArea.innerHTML = `
-          <h2>Form Values:</h2>
-          <hr>
-          <p>First Name: ${firstName}</p>
-          <p>Email: ${email}</p>
-          <p>Phone Number: ${phone}</p>
-          <p>Fruits: ${fruits.join(', ')}</p>
-          <p>Special Instructions: ${instructions}</p>
-          <br>
-          <h2>Nutrition Information</h2>
-          <hr>
-          <p>Carbohydrates: ${totalCarbs.toFixed(2)}</p>
-          <p>Protein: ${totalProtein.toFixed(2)}</p>
-          <p>Fat: ${totalFat.toFixed(2)}</p>
-          <p>Sugar: ${totalSugar.toFixed(2)}</p>
-          <p>Calories: ${totalCalories.toFixed(2)}</p>
-          <br>
-          <i>
-              <p>Order Date: ${orderDate}</p>
-          </i>
-        `;
-      })
+function submitForm(event) {
+  event.preventDefault();
 
-  });
-});
+  numDrinks++;
 
+  const eachCheckbox = document.getElementsByName("checkbox");
+  let allMarkedBoxes = [];
+  for (let i = 0; i < eachCheckbox.length; i++) {
+    const checkbox = eachCheckbox[i];
+    if (checkbox.checked) {
+      allMarkedBoxes.push(checkbox.value);
+      switch (checkbox.value) {
+        case fruit1.name:
+          totalCarbs += fruit1.nutritions.carbohydrates;
+          totalProtein += fruit1.nutritions.protein;
+          totalFat += fruit1.nutritions.fat;
+          totalSugar += fruit1.nutritions.sugar;
+          totalCalories += fruit1.nutritions.calories;
+          break;
+        case fruit2.name:
+          totalCarbs += fruit2.nutritions.carbohydrates;
+          totalProtein += fruit2.nutritions.protein;
+          totalFat += fruit2.nutritions.fat;
+          totalSugar += fruit2.nutritions.sugar;
+          totalCalories += fruit2.nutritions.calories;
+          break;
+        case fruit3.name:
+          totalCarbs += fruit3.nutritions.carbohydrates;
+          totalProtein += fruit3.nutritions.protein;
+          totalFat += fruit3.nutritions.fat;
+          totalSugar += fruit3.nutritions.sugar;
+          totalCalories += fruit3.nutritions.calories;
+          break;
+        default:
+          break;
+      }
+    }
+  }
 
+  const outputDiv = document.querySelector(".summary");
+  outputDiv.innerHTML = `
+    <br>
+    <h2>Form Values:</h2>
+    <hr>
+    <p>First Name: ${event.target.elements.fname.value}</p>
+    <p>Email: ${event.target.elements.email.value}</p>
+    <p>Phone Number: ${event.target.elements.phone.value}</p>
+    <p>Fruits: ${allMarkedBoxes.join(", ")}</p>
+    <p>Special Instructions: ${event.target.elements.instructions.value}</p>
+    <br>
+    <h2>Nutrition Information</h2>
+    <hr>
+    <p>Carbohydrates: ${totalCarbs.toFixed(2)}</p>
+    <p>Protein: ${totalProtein.toFixed(2)}</p>
+    <p>Fat: ${totalFat.toFixed(2)}</p>
+    <p>Sugar: ${totalSugar.toFixed(2)}</p>
+    <p>Calories: ${totalCalories.toFixed(2)}</p>
+    <br>
+    <i>
+      <p>Drink Mixes Ordered: ${numDrinks}</p>
+    </i>
+  `;
+
+  localStorage.setItem("numDrinks", numDrinks);
+}
